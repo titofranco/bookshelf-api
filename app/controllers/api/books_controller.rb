fb950a7ea10bd0fd863294af22594504c3b31531
Carlos Guisao Franco <carlos.guisao@globant.com>
2022-11-15 12:54:10 -0500

    Allow search books by query

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,13 +1,21 @@
 module Api
   class BooksController < ApplicationController
-    
+
     before_action :authorize!
     before_action :can_read_books!
 
     def index
-      books = Book.all
+      books = Book.order(:title)
+      books = books.where("title ILIKE ?", "%#{params[:query]}%") if params[:query]
       render json: books, each_serializer: BookSerializer
+    end
+
+    def show
+      book = Book.find_by(id: params[:id])
+      render json: book, serializer: BookSerializer
     end
   end
 
 end
+
+
