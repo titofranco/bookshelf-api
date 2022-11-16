812f9101bfb8e804da1fe9f037c74fb35ab40986
Carlos Guisao Franco <carlos.guisao@globant.com>
2022-11-16 09:17:13 -0500

    Add ListItemsController with actions

---
+++ app/controllers/api/list_items_controller.rb
@@ -0,0 +1,46 @@
+module Api
+  class ListItemsController < ApplicationController
+
+    before_action :authorize!
+
+    def index
+      list_items = ListItem.all
+      render json: list_items, each_serializer: ListItemSerializer
+    end
+
+    def create
+      list_item = ListItem.find_or_initialize_by(book_id: params[:bookId])
+      if list_item.valid?
+        list_item.save
+        render json: list_item.to_json(include: [:book]), status: 200
+      else
+        render json: {error: "Error saving list item"}, status: 400
+      end
+    end
+
+    def update
+      list_item = ListItem.find_by(id: params[:id])
+      if list_item
+        list_item.update(list_item_params)
+        render json: list_item.to_json(include: [:book]), status: 200
+      else
+        render json: {error: "Error updating list item"}, status: 400
+      end
+    end
+
+    def destroy
+      list_item = ListItem.find_by(id: params[:id])
+      if list_item
+        list_item.destroy
+        render json: {success: true}, status: :ok
+      end
+    end
+
+    private
+
+    def list_item_params
+      params[:list_item].delete(:id)
+      params.require(:list_item).permit(:book_id, :ownerId, :rating, :notes, :finishDate, :startDate)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   namespace :api do
-    resources :books, only: [:index]
+    resources :books, only: [:index, :show]
+    resources :list_items, only: [:index, :create, :update, :destroy]
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
